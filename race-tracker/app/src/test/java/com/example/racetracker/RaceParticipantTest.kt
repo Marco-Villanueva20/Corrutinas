c512f8eff71d100c13d68325d8fc7c87a1607c3e
Marco <marco_x1230@hotmail.com>
2025-04-10 13:32:22 -0500

    añadiendo el proyecto mars-photo

--- race-tracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ race-tracker/app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,13 +16,60 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class RaceParticipantTest {
+    // Se crea una instancia del participante de la carrera para las pruebas:
     private val raceParticipant = RaceParticipant(
-        name = "Test",
-        maxProgress = 100,
-        progressDelayMillis = 500L,
-        initialProgress = 0,
-        progressIncrement = 1
+        name = "Test",                // Nombre del participante para identificarlo en la prueba
+        maxProgress = 100,            // Progreso máximo para que la carrera finalice (100)
+        progressDelayMillis = 500L,   // Cada 500 milisegundos se actualiza el progreso
+        initialProgress = 0,          // Progreso inicial en 0
+        progressIncrement = 1         // Cada actualización incrementa el progreso en 1
     )
+
+    /*
+    * runTest: Un bloque que ejecuta la prueba dentro de un entorno controlado para corutinas.
+    * Permite simular el paso del tiempo y ejecutar las corutinas de forma determinista.
+    * advanceTimeBy(raceParticipant.progressDelayMillis)
+    * Avanza de forma simulada el tiempo en la prueba en 500 milisegundos,
+    * lo que hace que se "cumpla" el delay del primer ciclo del bucle en run().
+    *
+    * runCurrent()
+    * Ejecuta las tareas pendientes en la cola de corutinas que ya han quedado programadas.
+    * Esto asegura que la acción pendiente (el incremento del progreso) se ejecute inmediatamente.
+    * 
+    * assertEquals(exceptedProgress, raceParticipant.currentProgress)
+    * Se verifica que el progreso actual (currentProgress) sea igual al valor esperado (1).
+    * Esto valida que después de 500 ms de delay, el metodo run() hizo el incremento correcto.
+    *
+    * */
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdate() = runTest {
+        val exceptedProgress = 1 // Se espera que, después de un delay, el progreso sea 1
+        launch {
+            raceParticipant.run()
+        }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(exceptedProgress, raceParticipant.currentProgress)
+
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdate() = runTest {
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.maxProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(100, raceParticipant.currentProgress)
+    }
 }
